26b4a9fd4322d0a5e0fa1256bb473a3533128b08
Xin <66453714+ZhiXin826@users.noreply.github.com>
2020-12-02 12:16:35 +0800

    Create insertsomedata.php

---
+++ insertsomedata.php
@@ -0,0 +1,36 @@
+
+<?php
+
+// SQL Server Extension Sample Code:
+
+$connectionInfo = array("UID" => "apuadmin", "pwd" => "Apu@123456", "Database" => "simpleweb", "LoginTimeout" => 30, "Encrypt" => 1, "TrustServerCertificate" => 0);
+
+$serverName = "tcp:simplewebtp000001.database.windows.net,1433";
+
+$conn = sqlsrv_connect($serverName, $connectionInfo);
+
+if (!$conn)
+
+{
+
+die("Error connection: ".sqlsrv_errors());
+
+}
+
+
+
+$sql = "INSERT INTO restaurant(restaurant_name, restaurant_address, restaurant_phone) VALUES (?, ?, ?)";
+
+$params = array("7 Eleven", "Bukit Jalil", "03-12345678");
+
+
+
+$stmt = sqlsrv_query( $conn, $sql, $params);
+
+if( $stmt === false ) {
+
+die( print_r( sqlsrv_errors(), true));
+
+}
+
+?>
